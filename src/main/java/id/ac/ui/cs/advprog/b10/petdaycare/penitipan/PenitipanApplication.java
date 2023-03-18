cf1d80fa7fb6920b86a0f0c48da286b2fbfb1121
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-03-18 21:18:16 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/b10/petdaycare/penitipan/PenitipanApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b10.petdaycare.penitipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenitipanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenitipanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/b10/petdaycare/penitipan/PenitipanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.b10.petdaycare.penitipan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenitipanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
