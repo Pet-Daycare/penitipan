b1cf72815797b21d53e9533edda44fe42f6f4aec
Vicky Maulana <muhammad.vicky@ui.ac.id>
2023-05-10 18:58:55 +0700

    feat: initialize mailsender notification

--- src/test/java/id/ac/ui/cs/advprog/b10/petdaycare/penitipan/service/MailSenderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/b10/petdaycare/penitipan/service/MailSenderTest.java
@@ -1,5 +1,4 @@
 package id.ac.ui.cs.advprog.b10.petdaycare.penitipan.Service;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
